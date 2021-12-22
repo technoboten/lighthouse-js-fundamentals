efb69c0e07f2a820a5c7f77be5bf2e99ccd55c2e
modemgirl <serra@modemgirl.ca>
2021-12-22 23:20:26 +0000

    use conditionals to tell people how to dress

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+const temperature = -3;
+
+if(temperature < 0){
+  console.log("Make sure you pack a scarf");
+} else if (temperature < 15){
+  console.log("shorts won't cut it");
+} else {
+  console.log("shorts are fine.")
+}
+
+console.log("Now go outside");
