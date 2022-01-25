b2becb77b6b349efb39b38fbf76e454b93e857e5
serra <serra@modemgirl.ca>
2022-01-25 08:57:03 -0800

    adding caseConverter2

--- caseConverter2.js
+++ caseConverter2.js
@@ -1,52 +1,112 @@
-const makeCase = (input, styles) => {
+const makeCamel = (input) => {
   let result = "";
-  let style = "";
-  let sortedStyles = [];
-
-  if(Array.isArray(styles)){
-    // styles.forEach(el => console.log(`style is ${el}`));
-    for(let stylesIdx = 0; stylesIdx < styles.length; stylesIdx++){
-    //loop through stylesArray
-    console.log("styles loop");
-      //compare styles[stylesIdx] to casess
-      console.log(`style is: ${styles[stylesIdx]}`);
-      let caseIdx = 0;
-      let count = 0;
-      while(caseIdx <= cases.length){
-    //loop should run while ??
-        if(styles[stylesIdx] === cases[caseIdx]){
-          console.log(`case is: ${cases[caseIdx]}`);
-          console.log(`add to sortedStyles Array`);
-          sortedStyles.push(styles[stylesIdx]);
-          caseIdx = cases.length+2;
-        } else {
-          console.log(`case is: ${cases[caseIdx]}`);
-          console.log(`not a match, check the next case`);
-        }
-        caseIdx++ 
-      } 
-
-      console.log(sortedStyles);
-    }  
-  } else {
-    style = styles;
-      //result = applyStyle(input, style)
+  for(let i = 0; i < input.length; i++){
+  if(input[i] === " "){
+   i += 1;
+   result += input[i].toUpperCase();
+    } else {
+    result += input[i];
     }
+  }
+  return result;
+};
+
+const makePascal = (input) => {
+  let result = "";
+  result += input[0].toUpperCase();
+  for(let i = 1; i < input.length; i++){
+  if(input[i] === " "){
+   i += 1;
+   result += input[i].toUpperCase();
+    } else {
+    result += input[i];
+    }
+  }
+  return result;
+};
+
+const makeSnake = (input) => {
+  let result = "";
+  for(let i = 0; i < input.length; i++){
+  if(input[i] === " "){
+   result += "_";
+    } else {
+    result += input[i];
+    }
+  }
+  return result;
+};
+
+const makeKebab = (input) => {
+  let result = "";
+  for(let i = 0; i < input.length; i++){
+  if(input[i] === " "){
+   result += "-";
+    } else {
+    result += input[i];
+    }
+  }
+  return result;
+};
+
+// const makeTitle = (input) => {
+//   let result = "";
+//   for(let i = 0; i < input.length; i++){
+//   if(input[i] === " "){
+//    result += "-";
+//     } else {
+//     result += input[i];
+//     }
+//   }
+//   return result;
+// };
+
+const makeVowel = (input) => {
+  let result = "";
+  let vowels = ["a", "e", "i", "o", "u"];
+  for(let i = 0; i < input.length; i++){
+    for(let v = 0; v < vowels.length; v++){
+      if(input[i] === vowels[v]){
+        result += input[i].toUpperCase();
+        i = i + 1;
+      } 
+    }
+    result += input[i];
+  }
+  return result;
+};
+
+
+const makeCase = (input, style) => {
+  let output = "";
+  switch (style) {
+    case 'camel':
+      output = makeCamel(input);
+      break;
+    case 'pascal':
+      output = makePascal(input);
+      break;
+    case 'snake':
+      output = makeSnake(input);
+    case 'kebab':
+      output = makeKebab(input);
+      break;
+    case 'title':
+      // output = makeTitle(input);
+    case 'vowel':
+      output = makeVowel(input);
+      break;
+    case 'consonant':
+      // output = makeConsonant(input);
+    case 'upper':
+      // output = makeUpper(input);
+    case 'lower':
+      // output = makeLower(input);
+    break;
+    default: 
+      console.log(`invalid input`);
+  }
+  console.log(output);
   } 
 
-  let cases = ["camel", "pascal", "snake", "kebab", "title", "vowel", "consonant", "upper", "lower"];
-
-
-// const checkStyle = (style) => {
-//   if(style === "camel" || "pascal" || "snake" || "kebab" || "title"){
-//     console.log(`style is ${style}`);
-//   } else if (style === "vowel" || "consonant"){
-//     console.log(`style is ${style}`);
-//   } else if (style === "upper" || "lower"){
-//     console.log(`style is ${style}`);
-//   }
-// }
-
-//check all el in styles 
-
-makeCase("this is a string", ["pascal", "upper", "snake"]);
+  makeCase("this is a string", "kebab");
